cd9b037ba6d8c627f6469aac60fe8789e8f39cf1
Roel Sint <roel@beeldspraak.com>
2013-07-11 13:10:49 +0200

    Refactor DownloadController and add PhpcrDownloadController

---
+++ Controller/PhpcrDownloadController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Symfony\Cmf\Bundle\MediaBundle\Controller;
+
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+/**
+ * Controller to handle file downloads for things that have a route
+ */
+class PhpcrDownloadController extends AbstractDownloadController
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapPathToId($path)
+    {
+        // The path is being the id
+        $id = \PHPCR\Util\PathHelper::absolutizePath($path, '/');
+
+        if (0 !== strpos($id, $this->rootPath)) {
+            throw new \NotFoundHttpException(sprintf(
+                'The path "%s" is out of the root path "%s" were the file system is located.',
+                $path,
+                $this->rootPath
+            ));
+        }
+
+        return $id;
+    }
+}
