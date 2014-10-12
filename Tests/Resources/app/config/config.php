139e30bd70999fb81de41cd65f577838b0a04ede
Lukas Kahwe Smith <smith@pooteeweet.org>
2014-10-12 06:13:01 -0400

    fix test setup, added php 5.6

--- Tests/Resources/app/config/config.php
+++ Tests/Resources/app/config/config.php
@@ -1,5 +1,6 @@
 <?php
 
+$container->setParameter('cmf_testing.bundle_fqn', 'Symfony\Cmf\Bundle\MediaBundle');
 $loader->import(CMF_TEST_CONFIG_DIR . '/default.php');
 $loader->import(CMF_TEST_CONFIG_DIR . '/phpcr_odm.php');
 $loader->import(__DIR__.'/cmf_media.yml');
