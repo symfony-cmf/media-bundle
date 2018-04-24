31d2a3df660e3dbc4bacc9cf32b0ee9695efb931
Kim Wuestkamp <kim@wuestkamp.com>
2018-04-24 11:54:31 +0200

    make PhpcrDriver work with helios-ag/fm-elfinder-bundle version 8
    helios-ag/fm-elfinder-bundle uses Studio-42/elFinder instead of helios-ag/ElFinderPHP (https://github.com/helios-ag/FMElfinderBundle/releases/tag/8.0)

--- src/Adapter/ElFinder/PhpcrDriver.php
+++ src/Adapter/ElFinder/PhpcrDriver.php
@@ -15,7 +15,6 @@ use Doctrine\Common\Persistence\ManagerRegistry;
 use Doctrine\ODM\PHPCR\Document\Generic;
 use Doctrine\ODM\PHPCR\Document\Resource;
 use Doctrine\ODM\PHPCR\DocumentManager;
-use FM\ElFinderPHP\Driver\ElFinderVolumeDriver;
 use Imagine\Filter\ FilterInterface;
 use PHPCR\Util\PathHelper;
 use Symfony\Cmf\Bundle\MediaBundle\DirectoryInterface;
@@ -31,7 +30,7 @@ use Symfony\Cmf\Bundle\MediaBundle\Templating\Helper\CmfMediaHelper;
 /**
  * @author Sjoerd Peters <sjoerd.peters@gmail.com>
  */
-class PhpcrDriver extends ElFinderVolumeDriver
+class PhpcrDriver extends \elFinderVolumeDriver
 {
     /**
      * Driver id
